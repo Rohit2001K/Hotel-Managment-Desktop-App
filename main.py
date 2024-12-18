1b2966970dfc435ed4725cbfe0504652943f01b2
Rohit Kumawat <rohitkumavatgame@gmail.com>
2024-12-18 15:15:55 +0530

    Updated code

--- main.py
+++ main.py
@@ -2,48 +2,29 @@ import tkinter
 from tkinter import *
 
 
-#config
+class login:
+    def __init__(self,root):
+
+
+        #basic config
+        self.root=root
+        self.root.title("XYZ")
+        self.root.geometry("800x400")
+
+        #messages
+        self.welcome_msg=Label(root,text="Welcome",font=("", 25)).pack(pady=10)
+        self.login_msg=Label(root,text="Please Enter Your UserID And Password Below").pack(pady=10)
+
+        #userid and password
+
+
+
+
+
+
+
+
 root=tkinter.Tk()
-root.title("XYZ Motel")
-root.geometry("800x400")
 
-frame=tkinter.Frame(root)
-frame.pack(pady=10)
-
-
-#welcome
-welcome_msg=Label(root,text="Welcome",font=("", 25)).pack(pady=10)
-login_msg=Label(root,text="Please Enter Your UserID And Password Below").pack(pady=10)
-
-
-
-#login
-user_id_text=Label(root,text="Enter UserID :").pack()
-user_id=Entry(root,width=50).pack()
-
-
-password_text=Label(root,text="Enter Password :").pack()
-password=Entry(root,width=50).pack()
-
-login_button=Button(root,text="Login").pack()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+hotel=login(root)
 root.mainloop()
